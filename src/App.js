892c2afa737044002e7bb59b479282aeb7e25fc2
emanuel gimenez <emanuelgimenez2@gmail.com>
2022-03-20 17:29:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Boton from "./components/Boton";
+import Contador from "./components/Contador";
+import {useState} from "react";
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+   setNumClics(numClics + 1);
+  };
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+  const bajarContador = () => {
+    setNumClics(numClics - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="titulo-contenedor">
+        <h1>
+          Contador
+        </h1>
+      </div>
+
+    <div className="contendor">
+
+        <Contador numClics={numClics} /> 
+        
+
+        <Boton 
+        texto="Subir" 
+        esBotonDeClic={true} 
+        manejarClic={manejarClic} 
+
+        />
+        <Boton
+          texto="Bajar"
+          esBotonDeClic={false}
+          manejarClic={bajarContador}
+        />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+
+    </div>
     </div>
   );
 }
