96aabbccdcd879afdfe3b57bc533f5285e7ad1da
emanuel gimenez <emanuelgimenez2@gmail.com>
2022-04-17 14:34:49 -0300

    finalizacion proyecto de demostracion del uso del useState

--- src/App.js
+++ src/App.js
@@ -2,41 +2,31 @@ import "./App.css";
 import React from "react";
 import Boton from "./components/Boton";
 import Contador from "./components/Contador";
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
-
   const [numClics, setNumClics] = useState(0);
 
   const manejarClic = () => {
-   setNumClics(numClics + 1);
+    setNumClics(numClics + 1);
   };
   const reiniciarContador = () => {
     setNumClics(0);
   };
   const bajarContador = () => {
     setNumClics(numClics - 1);
-  }
+  };
 
   return (
     <div className="App">
       <div className="titulo-contenedor">
-        <h1>
-          Contador
-        </h1>
+        <h1>Contador</h1>
       </div>
 
-    <div className="contendor">
+      <div className="contendor">
+        <Contador numClics={numClics} />
 
-        <Contador numClics={numClics} /> 
-        
-
-        <Boton 
-        texto="Subir" 
-        esBotonDeClic={true} 
-        manejarClic={manejarClic} 
-
-        />
+        <Boton texto="Subir" esBotonDeClic={true} manejarClic={manejarClic} />
         <Boton
           texto="Bajar"
           esBotonDeClic={false}
@@ -47,8 +37,7 @@ function App() {
           esBotonDeClic={false}
           manejarClic={reiniciarContador}
         />
-
-    </div>
+      </div>
     </div>
   );
 }
